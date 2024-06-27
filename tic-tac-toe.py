e7a4b1761a7b7caad239ccc3f2f5d523437ad3b4
Kartik0211 <141504617+Kartik0211@users.noreply.github.com>
2024-06-27 15:11:03 +0530

    Add files via upload

---
+++ tic-tac-toe.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tic-Tac-Toe")
+        self.board = [" " for _ in range(9)]
+        self.current_player = "X"
+        self.buttons = []
+        self.create_board()
+
+    def create_board(self):
+        for i in range(9):
+            button = tk.Button(self.root, text=" ", font=("Helvetica", 20), width=5, height=2,
+                               command=lambda i=i: self.on_button_click(i))
+            button.grid(row=i // 3, column=i % 3)
+            self.buttons.append(button)
+
+    def on_button_click(self, index):
+        if self.board[index] == " ":
+            self.board[index] = self.current_player
+            self.buttons[index].config(text=self.current_player)
+            if self.check_win():
+                messagebox.showinfo("Tic-Tac-Toe", f"Player {self.current_player} wins!")
+                self.reset_game()
+            elif self.check_draw():
+                messagebox.showinfo("Tic-Tac-Toe", "It's a draw!")
+                self.reset_game()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_win(self):
+        win_conditions = [
+            [self.board[0], self.board[1], self.board[2]],
+            [self.board[3], self.board[4], self.board[5]],
+            [self.board[6], self.board[7], self.board[8]],
+            [self.board[0], self.board[3], self.board[6]],
+            [self.board[1], self.board[4], self.board[7]],
+            [self.board[2], self.board[5], self.board[8]],
+            [self.board[0], self.board[4], self.board[8]],
+            [self.board[2], self.board[4], self.board[6]]
+        ]
+        return [self.current_player, self.current_player, self.current_player] in win_conditions
+
+    def check_draw(self):
+        return " " not in self.board
+
+    def reset_game(self):
+        self.board = [" " for _ in range(9)]
+        for button in self.buttons:
+            button.config(text=" ")
+        self.current_player = "X"
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
